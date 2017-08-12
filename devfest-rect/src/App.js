4110ca3b93186f77754f8254853f505da1f02150
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-08-12 14:00:00 +0200

    Add React version

---
+++ devfest-rect/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './components/calculator';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      formula: '',
+    }
+  }
+  
+  updateFormula(formula){
+    this.setState({
+      formula,
+    });
+  }
+  
+  render() {
+    return (
+      <div id="background">
+             
+        <div id="result">{this.state.formula}</div>
+         <Calculator updateFormula={ (formula) => this.updateFormula(formula) }/>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ devfest-rect/src/components/calculator.jsx
@@ -0,0 +1,175 @@
+import React, {Component} from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        formula: '',
+    }
+  }
+
+  clickDelete() {
+    this.setState({ formula: ''})
+    this.props.updateFormula(this.state.formula);
+  }
+
+  clickSign(e) {
+    const previousResult = eval(this.state.formula);
+    const sign = e.target.value;
+    const result = sign !== "=" ? previousResult + e.target.value : previousResult;
+    this.setState({ formula: result });
+    this.props.updateFormula(result);
+  }
+
+  clickNumber(e) {
+    const result = this.state.formula + e.target.value;
+    this.setState({ formula: result})  
+    this.props.updateFormula(result);
+  }
+  render() {
+    return (
+      <div id="main">
+        <div id="first-rows">
+          <button onClick={e => this.clickDelete(e)} className="del-bg" id="delete">
+            Del
+          </button>
+          <button
+            value="%"
+            onClick={e => this.clickSign(e)}
+            className="btn-style operator opera-bg fall-back"
+          >
+            %
+          </button>
+          <button
+            value="+"
+            onClick={e => this.clickSign(e)}
+            className="btn-style opera-bg value align operator"
+          >
+            +
+          </button>
+        </div>
+
+        <div className="rows">
+          <button
+            value="7"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num first-child"
+          >
+            7
+          </button>
+          <button
+            value="8"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num"
+          >
+            8
+          </button>
+          <button
+            value="9"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num"
+          >
+            9
+          </button>
+          <button
+            value="-"
+            onClick={e => this.clickSign(e)}
+            className="btn-style opera-bg operator"
+          >
+            -
+          </button>
+        </div>
+
+        <div className="rows">
+          <button
+            value="4"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num first-child"
+          >
+            4
+          </button>
+          <button
+            value="5"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num"
+          >
+            5
+          </button>
+          <button
+            value="6"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num"
+          >
+            6
+          </button>
+          <button
+            value="*"
+            onClick={e => this.clickSign(e)}
+            className="btn-style opera-bg operator"
+          >
+            x
+          </button>
+        </div>
+
+        <div className="rows">
+          <button
+            value="1"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num first-child"
+          >
+            1
+          </button>
+          <button
+            value="2"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num"
+          >
+            2
+          </button>
+          <button
+            value="3"
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg num"
+          >
+            3
+          </button>
+          <button
+            value="/"
+            onClick={e => this.clickSign(e)}
+            className="btn-style opera-bg operator"
+          >
+            /
+          </button>
+        </div>
+
+        <div className="rows">
+          <button
+            value="0"
+            onClick={e => this.clickNumber(e)}
+            className="num-bg zero"
+            id="delete"
+          >
+            0
+          </button>
+          <button
+            value="."
+            onClick={e => this.clickNumber(e)}
+            className="btn-style num-bg period fall-back"
+          >
+            .
+          </button>
+          <button
+            value="="
+            onClick={e => this.clickSign(e)}
+            id="eqn-bg"
+            className="eqn align"
+          >
+            =
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
